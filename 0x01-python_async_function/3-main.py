7a215334a73122398e64ba4695cefe5e12abba3c
israel-09 <israelinene91@gmail.com>
2024-03-20 23:59:38 +0100

    task 3 solution

---
+++ 0x01-python_async_function/3-main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import asyncio
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def test(max_delay: int) -> float:
+    task = task_wait_random(max_delay)
+    print(task.__class__)
+    await task
+
+asyncio.run(test(5))
+
